d1b63887ebd4c42b33c41896af49b9bed18cf0f2
majorcodeX <neon.ratemo@gmail.com>
2025-05-13 17:04:02 +0300

    halfway

--- e-tab.py
+++ e-tab.py
@@ -1,8 +1,15 @@
 greeting = input("Hello, welcome to the Rockysoul hotel, would you like to make an order yourself with this tablet or would you like a waiter to come over? (tablet / waiter): ").lower()
 
 if greeting == "tablet":
-    choice = input("Great, thank you for choosing me, now we have quite a selection of flavours for you today. Would you like to start with a drink, an appetizer or would you prefer jumping straight to the main meal? (appetizer / main meal): ")
-    #if choice == ""
+    choice = input("Great, thank you for choosing me, now we have quite a selection of flavours for you today. Would you like to start with a drink, an appetizer or would you prefer jumping straight to the main meal? (appetizer / drink / main meal): ").lower()
+    if choice == "appetizer":
+        appetite = input("Let's see what appetizer you can have today, we have the following on our menu: Beef Carpaccio, ice cream, Tuna Tartare, Burrata Salad, Smoked Salmon Blinis, Mushroom Risotto, Duck spring rolls. Please select what you would like to have:  ").lower()
+        print(f"Great choice, we will have your {appetite} coming right up ")
+    elif choice == "drink":
+        drink = input("Would you like an alcoholic drink or a non-alcoholic drink? (alcoholic / non-alcoholic): ").lower()
+        
+    else: 
+        main = input("Wow, you must be starving today, luckily we have just the right mouth savouring dishes, we have: ").lower()
 
 else:
     if greeting == "waiter":
